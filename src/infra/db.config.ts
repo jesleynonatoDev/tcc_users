8b8f57d0ef64db566286813a4acc6c98f14d2316
jesleynonatoDev <jesleynonato@hotmai.com>
2023-06-29 00:03:01 -0300

    feat(infra): refactor

--- src/infra/db.config.ts
+++ src/infra/db.config.ts
@@ -5,10 +5,9 @@ const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 export const db = {
   init: () => {
-    logger.info(`mongodb://${DB_USER}:${DB_PASS}@db:${DB_PORT}/${DB_NAME}`);
         mongoose
           .connect(
-            `mongodb://${DB_USER}:${DB_PASS}@db:${DB_PORT}/${DB_NAME}?authSource=admin`
+            `mongodb://${DB_USER}:${DB_PASS}@db_users:${DB_PORT}/${DB_NAME}?authSource=admin`
           )
           .then((res) => logger.info(`Connection Succesful ${res}`))
           .catch((err) => logger.error(`Erro in DB connection: ${err}`));
